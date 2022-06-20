015e27581aa20131411e94a3af53a7011340617c
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-06-20 11:16:22 -0700

    map recipe, react packages

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
 
   useEffect(() => {
     getPopular();
@@ -9,12 +12,17 @@ function Popular() {
   const getPopular = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes)
   }
 
   return (
     <div>
-      <h1> Popluar </h1>
+      {popular.map((recipe => {
+        return(
+          <div key={recipe.id}>
+            {recipe.title} </div>
+        );
+      }))}
     </div>
   )
 }
