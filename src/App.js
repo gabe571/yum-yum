c8a82296130e8dd0dfc45cb4425704212286c1bc
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-07-07 08:23:37 -0700

    BrowserRouter - Navlink working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Pages from "./pages/Pages";
 import Catagory from "./components/Catagory";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
         <Catagory />
         <Pages />
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cuisine() {
+  return (
+    <div>
+      <h1> Cuisine </h1>
+    </div>
+  )
+}
+
+export default Cuisine
