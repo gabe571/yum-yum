bfae28b5a2eb627a4acb614644b01450089f2540
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-07-07 08:37:26 -0700

    cusine fetch, useEffect params

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Cuisine() {
+
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    //fetchings recipes based of name
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+        
+    }
+
+    // dynamically update cuisine page, using params.type
+    useEffect(() => {
+        getCuisine(`params.type`)
+        console.log(params.type)
+    }, [params.type])
   return (
     <div>
-      <h1> Cuisine </h1>
+        
     </div>
   )
 }
