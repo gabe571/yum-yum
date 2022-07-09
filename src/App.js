e8d6176bca3a8307e8aa52ba83442cdb2513b79d
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-07-09 11:49:00 -0700

    search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./pages/Pages";
 import Catagory from "./components/Catagory";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Search />
         <Catagory />
         <Pages />
         </BrowserRouter>
